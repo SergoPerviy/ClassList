14ac2a2e56a1b306dbf09295cc9e3410001702fa
mrdan <mrdan@NETLOGIC>
2024-07-24 21:18:55 +0500

    Добавьте файлы проекта.

---
+++ ClassList/Main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "List.hpp"
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	List<int> list1;
+
+	list1.push_back(15);
+	list1.push_back(7);
+	list1.push_front(42);
+	//list1.clear();
+	list1.print();
+	// list1.reverse_print();
+
+	//list1.pop_back();
+	list1.pop_front();
+	list1.print();
+
+	List<std::string> list2{ "Hello", "World", "Dabula", "Chada" };
+	list2.print();
+
+	return 0;
+}
